74f8911c35b8ca34c3eec4ae06513d76f0086b22
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-05 14:13:05 +0530

    FIRST CODE OF OOPs

---
+++ OOPs/Basic.java
@@ -0,0 +1,13 @@
+class A {
+    void show() {
+        System.out.println("hello World");
+    }
+}
+
+public class Basic {
+    public static void main(String[] args) {
+        // class object
+        A p = new A();
+        p.show();
+    }
+}
