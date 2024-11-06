21f5b18f833e14584b3cbb507f2361c95a4fbcd3
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-06 14:16:29 +0530

    EXAMPLE OF METHOD

---
+++ OOPs/ExampleOfCovariant.java
@@ -0,0 +1,37 @@
+class A {
+    int show() {
+        int a = 98;
+        return a;
+    }
+
+    // covariant return type
+    A show1() {
+        System.out.println("covariant A class");
+        return this;
+    }
+
+}
+
+class B {
+    int show() {
+        int num = 67;
+        return num;
+    }
+
+    B hello() {
+        System.out.println("b class covariant");
+        return this;
+    }
+}
+
+public class ExampleOfCovariant {
+    public static void main(String[] args) {
+        A p=new A();
+        System.out.println(p.show());
+        p.show1();
+
+        B k=new B();
+        System.out.println(k.show());
+        k.hello();
+    }
+}
