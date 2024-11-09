3b41d26072db48902e01580f55b827cd767c7edf
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-09 13:58:43 +0530

    DEFAULT CONSTRUCTOR BASIC CODE

---
+++ OOPs/ConstructorBasic.java
@@ -0,0 +1,16 @@
+//create class 
+class A {
+    // make constructor :default constructor
+    A() {
+        System.out.println("hello world");
+    }
+}
+
+public class ConstructorBasic {
+    public static void main(String[] args) {
+        //call two ways : 
+        new A();//first way         
+        //second way
+        A p=new A(); //second way 
+    }
+}
