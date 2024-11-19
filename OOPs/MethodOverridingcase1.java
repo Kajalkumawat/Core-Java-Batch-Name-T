a594f47bdcaa9921444a2c473571f0d9169f6d86
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-19 14:25:11 +0530

    THIS PROBLEM IS NOT METHOD OVERRIDING

---
+++ OOPs/MethodOverridingcase1.java
@@ -0,0 +1,18 @@
+//this problem is not method overriding 
+class A {
+    void show(int a) {
+        System.out.println(a);
+    }
+}
+
+class B extends A {
+    void show() {
+        System.out.println("hello world");
+    }
+}
+
+public class MethodOverridingcase1 {
+    public static void main(String[] args) {
+
+    }
+}
