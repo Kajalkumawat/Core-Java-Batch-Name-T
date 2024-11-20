5e20c3a25e95bfa5e7c2288a3032e9dd24da781d
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-20 14:12:59 +0530

    METHOD OVERHIDING IN JAVA

---
+++ OOPs/MethodOverhiding.java
@@ -0,0 +1,16 @@
+class A{
+   static void show(){
+        System.out.println("hello");
+    } 
+}
+class B extends A{
+    static  void show(){
+        System.out.println("world");
+    }
+}
+public class MethodOverhiding {
+    public static void main(String[] args) {
+        B p=new B();
+        p.show();
+    }
+}
