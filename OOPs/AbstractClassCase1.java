fee741dbe96be370dfe15b786aef9c5fbee0fb99
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-20 14:39:17 +0530

    abstract class and method in java

---
+++ OOPs/AbstractClassCase1.java
@@ -0,0 +1,16 @@
+abstract class A {
+    abstract void show();// abstract method always declare
+}
+
+class B extends A {
+    void show() {
+        System.out.println("abstract method used ");
+    }
+}
+
+public class AbstractClassCase1 {
+    public static void main(String[] args) {
+        B p = new B();
+        p.show();
+    }
+}
