ead0e9bb3a533b05a4ef3e0cecf4f9d6c84e48f0
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-22 14:13:41 +0530

    OBJECT CLASS IN JAVA

---
+++ OOPs/ObjectClass.java
@@ -0,0 +1,12 @@
+//normal class :child class internally 
+class A extends Object {
+
+}
+class B extends A{
+    //why ? B class is not child class of object class because multiple inheritance does not support in java :diamond problem faced (complexity increase )
+}
+public class ObjectClass {
+    public static void main(String[] args) {
+
+    }
+}
