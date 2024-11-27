2ec6a4f2e21893b37c146a3aba11a8b387317073
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-27 14:50:40 +0530

    METHOD CHAINING IN JAVA

---
+++ OOPs/MethodChaining.java
@@ -0,0 +1,20 @@
+class A {
+    void show1() {
+        System.out.println("show1 method");
+    }
+
+    void show2() {
+        show1();
+    }
+
+    void show3() {
+        show2();
+    }
+}
+
+public class MethodChaining {
+    public static void main(String[] args) {
+        A p = new A();
+        p.show3();
+    }
+}
