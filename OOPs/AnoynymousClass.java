3155d7e2b7a81ab190b637652662fc1690327e24
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-05 14:19:22 +0530

    anonymous inner class

---
+++ OOPs/AnoynymousClass.java
@@ -0,0 +1,24 @@
+    abstract class A {
+        abstract void show();
+    }
+
+    interface B {
+        void show1();
+    }
+
+    public class AnoynymousClass {
+        public static void main(String[] args) {
+            A k = new A() {
+                void show() {
+                    System.out.println("abstract method");
+                }
+            };
+            k.show();
+            B p = new B() {
+                public void show1() {
+                    System.out.println("interface method");
+                }
+            };
+            p.show1();
+        }
+    }
