6d28eb0fa5a4987199f93b2cedbc4c771de9e24d
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-09 14:36:08 +0530

    C file

---
+++ OOPs/C.java
@@ -0,0 +1,11 @@
+class A {
+    void show1() {
+        System.out.println("A class method");
+    }
+}
+
+class C extends A {
+    void show2() {
+        System.out.println("c class method");
+    }
+}
