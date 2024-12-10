c6b4d7aadb1fbd4792a7791377edb462c577ac05
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-10 14:21:22 +0530

    automatic typecasting in java

---
+++ OOPs/Basic1.java
@@ -0,0 +1,15 @@
+public class Basic1 {
+    public static void main(String[] args) {
+        byte a = 78;
+        short b = a;
+        System.out.println(b);
+
+        byte c = 23;
+        float d = c;
+        System.out.println(d);
+
+        // short e = 89;
+        // char f = e;
+        // System.out.println(f);
+    }
+}
