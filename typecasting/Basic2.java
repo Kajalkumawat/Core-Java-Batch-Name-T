030a2468ac31ea179d0ce56ad5298f48e1f68c06
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-10 14:37:05 +0530

    manually typecasting in java

---
+++ typecasting/Basic2.java
@@ -0,0 +1,13 @@
+public class Basic2 {
+    public static void main(String[] args) {
+        double a = 67.4;
+        float b = (float) a;
+        System.out.println(b);
+
+        // float -- int --- double
+        float c = 78.6f;
+        int d = (int) c;
+        double e = d;
+        System.out.println(e);
+    }
+}
