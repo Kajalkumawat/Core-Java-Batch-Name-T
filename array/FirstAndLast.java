0cabad87eabe889c7f7f4415f1c1da4e34398eea
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-12 14:58:45 +0530

    first and last element of the array

---
+++ array/FirstAndLast.java
@@ -0,0 +1,6 @@
+public class FirstAndLast {
+    public static void main(String[] args) {
+        int a[] = { 56, 64, 32, 56, 12, 18 };
+        System.out.println(a[0] + " " + a[a.length - 1]);
+    }
+}
