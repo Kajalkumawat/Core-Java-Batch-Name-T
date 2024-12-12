c67da7f5108c3e974bda043867bc30a56e347b81
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-12 14:46:24 +0530

    second way to create array

---
+++ array/CreateArrayAnotherWay.java
@@ -0,0 +1,11 @@
+public class CreateArrayAnotherWay {
+    public static void main(String[] args) {
+        //12,78,54,34 
+        int a[]=new int[5];//5 :size  
+        a[0]=12;
+        a[1]=78;
+        a[2]=54;
+        a[4]=34;
+        System.out.println(a[3]);
+    }
+}
