3f4261343172f0efc48db7ddc75277f32a1ee9e3
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-12 14:37:35 +0530

    create array in java

---
+++ array/CreationArray.java
@@ -0,0 +1,7 @@
+public class CreationArray {
+    public static void main(String[] args) {
+        // 12,78,54,34
+        int a[] = { 12, 78, 54, 34 };
+        System.out.println(a[3]);
+    }
+}
