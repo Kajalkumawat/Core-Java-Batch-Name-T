665894592a8eaa6d87e49caf9edc30f70e689d9a
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-12 14:51:58 +0530

    length of the  array in java

---
+++ array/LengthOfArray.java
@@ -0,0 +1,7 @@
+public class LengthOfArray {
+    public static void main(String[] args) {
+        //predefine variable :length variable 
+        int a[]={23,65,22,33,44,34,13,56};
+        System.out.println(a.length);
+    } 
+}
