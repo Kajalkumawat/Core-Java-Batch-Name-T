8fce73f3d9f6f428d72656334104d09d828b7b47
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-19 14:36:57 +0530

    find maximum number in array

---
+++ array/FindMax.java
@@ -0,0 +1,12 @@
+public class FindMax {
+    public static void main(String[] args) {
+        int a[] = { 1, 4, 3, 5, 2 };
+        int max = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
