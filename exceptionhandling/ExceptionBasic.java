bf43182f72a2715a80635514a0db3f20ebbed3da
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-01-08 16:37:10 +0530

    basic code of exception

---
+++ exceptionhandling/ExceptionBasic.java
@@ -0,0 +1,9 @@
+public class ExceptionBasic{
+    public static void main(String[] args) {
+        int a=89;
+        System.out.println(a);
+        System.out.println("exception handling code in java ");
+        int b=8/0;
+        System.out.println(b);
+    }
+}
