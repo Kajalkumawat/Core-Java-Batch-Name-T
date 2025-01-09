a6c184af2fa6b445568a18858e747b2c6f30dbc8
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-01-09 17:08:22 +0530

    number format exception in java

---
+++ exceptionhandling/Exceptions2.java
@@ -0,0 +1,10 @@
+public class Exceptions2 {
+    public static void main(String[] args) {
+        //number format exception : string  convert int 
+        String s="abc";
+        //s convert int :predefine method Integer.parseInt() 
+        Integer a=Integer.parseInt(s);
+        System.out.println(a);
+
+    }
+}
