df587cfe9bde5768ccdc541bd76d647332381f74
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-01-30 13:15:33 +0530

    throw case 4

---
+++ exceptionhandling/ThrowCase4.java
@@ -0,0 +1,10 @@
+public class ThrowCase4 {
+    public static void main(String[] args) {
+       
+        throw new ArithmeticException("hello ");
+       // System.out.println("world");
+       /*
+        * what is unreachable statement error :when print message after using throw 
+        */
+    }
+}
