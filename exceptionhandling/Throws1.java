c2a867ccb8aaaa2487274f18c64ce5d371783521
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-01-31 13:17:40 +0530

    throw basic code

---
+++ exceptionhandling/Throws1.java
@@ -0,0 +1,11 @@
+class A{
+    void show() throws ArithmeticException{
+        System.out.println("hello");
+    }
+}
+public class Throws1 {
+    public static void main(String[] args) {
+        A p=new A();
+        p.show();
+    }
+}
